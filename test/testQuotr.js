21a5ebde6e3535097e8c021da0711e0c9ba528fc
Solo Traze Dev <traze.solo.dev@gmail.com>
2016-06-25 21:19:38 +0530

    Added test suite for quotr.js

---
+++ test/testQuotr.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert;
+var quotr = require('../quotr/quotr');
+
+describe('quotr.js', function(){
+  describe('Using povider IC', function(){
+    it('Should be able to fetch quote data in JSON', function(done){
+
+      done(); // Mark test complete
+    });
+  });
+});
