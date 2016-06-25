e5d3952acaeb2af9c1133b00b9adff3322161c6f
Solo Traze Dev <traze.solo.dev@gmail.com>
2016-06-26 00:12:00 +0530

    quotr.js with request function and test suite

--- test/testQuotr.js
+++ test/testQuotr.js
@@ -1,11 +1,32 @@
 var assert = require('chai').assert;
 var quotr = require('../quotr/quotr');
 
+/* Configure test env */
+var webTestTimeout = 5000; // Seconds for test timeout if a web request is involved
+/* Test Data */
+var stockCode = 'ALLBAN';
+
 describe('quotr.js', function(){
   describe('Using povider IC', function(){
-    it('Should be able to fetch quote data in JSON', function(done){
+    it('should be able to fetch quote data', function(done){
+      this.timeout(webTestTimeout);
+      quotr.getQuote(stockCode, function(err, data) {
+        if(err) { done(err); return; }
+        assert.isNotNull(data, 'Quote data returned was null');
+        done(); // Mark test complete
+      });
+    });
 
-      done(); // Mark test complete
+    it('should provide quote data in expected JSON structure', function(done){
+      this.timeout(webTestTimeout);
+      quotr.getQuote(stockCode, function(err, data) {
+        if(err) { done(err); return; }
+        assert.isNotNull(data.nse, 'NSE data empty');
+        assert.isNumber(data.nse.lastTradedPrice, 'NSE data invalid');
+        assert.isNotNull(data.bse, 'BSE data empty');
+        assert.isNumber(data.bse.lastTradedPrice, 'BSE data invalid');
+        done(); // Mark test complete
+      });
     });
   });
 });
