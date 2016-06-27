6452586ec1622dcd2e9e7b1efd2d40194b223a04
Solo Traze <traze.solo.dev@gmail.com>
2016-06-28 00:54:47 +0530

    Interactive app

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var quotr = require('./quotr/quotr');
+var stdin = process.openStdin();
 
-quotr.getQuote('ALLBAN', function(err, data) {
-  if (err) {
-    console.log('Failed. Error: '+err);
-  }
-  else {
-    console.log(JSON.stringify(data,null,2));
-  }
+console.log('Enter stock code: ');
+stdin.addListener("data", function(d) {
+  var code = d.toString().trim();
+
+  quotr.getQuote(code, function(err, data) {
+    if (err) {
+      console.log('Failed. Error: '+err);
+    }
+    else {
+      //console.log(JSON.stringify(data,null,2));
+      console.log('Last Trade:\nNSE: ' + data.nse.lastTradedPrice + '\nBSE: ' + data.bse.lastTradedPrice);
+    }  
+    process.exit(0);
+  });
 });
